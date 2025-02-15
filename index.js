b781d345587764d433d9636e2a494b14484e4bc6
shivam gupta <shivamkashyap9198@gmail.com>
2025-02-16 01:03:03 +0530

    fix bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors( {  
-  origin : ["http://localhost:5173","https://accredian-backend-6au9.onrender.com/api/referrals"], 
+  origin : ["http://localhost:5173","https://accredian-frontend-wydu.vercel.app"], 
     credentials : true ,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
