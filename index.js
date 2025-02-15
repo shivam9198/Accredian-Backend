24c15c8aac56b6e3f0bcdc291983c15a4899fa5c
shivam gupta <shivamkashyap9198@gmail.com>
2025-02-15 23:18:29 +0530

    fix bugs.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors( {  
-  origin : "http://localhost:5173", // Allows all origins (for testing)
+  origin : "http://localhost:5173", 
     credentials : true ,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
